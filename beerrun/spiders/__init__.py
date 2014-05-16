83603ce6685a940702f05602166142e91a79086f
tejbirwason <tejbirwason@gmail.com>
2014-05-16 15:21:35 -0400

    second commit

--- beerrun/spiders/__init__.py
+++ beerrun/spiders/__init__.py
@@ -5,18 +5,80 @@
 import pymongo
 from pymongo import MongoClient
 from scrapy.spider import Spider
+from scrapy.spider import Request
 from bs4 import BeautifulSoup
 import re
+import requests
 
 
 class BeerRunSpider(Spider):
     name = "beerrun"
     allowed_domains = ["thebeerstore.ca"]
     start_urls = [
-        "http://www.thebeerstore.ca/beers/inventory/154/2371"
+        "http://www.thebeerstore.ca/stores"
     ]
 
+
     def parse(self, response):
+        soup = BeautifulSoup(response.body)  
+        store_name=[]
+        store_id=[]
+
+        ## mongo stuff
+        client = MongoClient()
+        db = client.mydb
+        beerrun = client.beerrun
+
+
+        for link in soup.find_all("a","store-link"):
+            extractStoreName = link.get_text() 
+            store_name.append(extractStoreName)
+            extractID = re.findall(r'\d+',str(re.findall(r'# \d+',str(link.parent()))))[0]
+            store_id.append(extractID)
+            
+            #print beer
+            showcaseURL = "http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id="+extractID
+            soup = BeautifulSoup((requests.get(showcaseURL)).text)
+            count = 0
+            for link in soup.find_all("a","brand-link teaser"):
+                beerinventoryURL = "http://www.thebeerstore.ca"+link.get('href')
+                imageURL = (link.find_all("img","image-style-none")[0]).get('src')
+                #print(link.get('href'))
+                beer = { "store_name": extractStoreName,
+                    "store_id": extractID,
+                    "showcase_url":showcaseURL,
+                    "beer_invetory_url": beerinventoryURL
+                    "brand_image_url": imageURL}
+                #db.beerrun.insert(beer)
+            #print re.findall(r'\d+',str(re.findall(r'# \d+',str(link.parent()))))[0]
+
+        #print db.collection.find()
+        #break
+        #print store_name
+        #print store_id
+
+        #for storeIdAppendLink in store_id:
+            #beerlist = []
+           # returned = requests.get("http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id="+str(storeIdAppendLink))
+          #  soup = BeautifulSoup(returned.text)
+            #for link in soup.find_all("a","brand-link teaser"):
+                #print(link.get('href'))
+
+
+
+    def here(self, response):
+        print "here"
+
+        #r = requests.get("http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id=4607")
+        #print(r.text)
+        #print(r.status_code)
+        #soup.find_all('a',)
+
+        #tryin = Request(url="http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id=4607",
+                              # cookies={'currency': 'USD', 'country': 'UY'})
+        #print(soup.prettify())
+
+    def parse2(self, response):
         soup = BeautifulSoup(response.body)        
         a=[]
         b=[]
@@ -36,7 +98,6 @@ class BeerRunSpider(Spider):
         for inventory in soup.find_all("td","inventory"):
             d.append(int(inventory.get_text()))
         
-        
         e=[]
         
         assert len(a) == len(b) == len(c)
@@ -48,8 +109,9 @@ class BeerRunSpider(Spider):
                 else:
                     e.append(a[i]/b[i])
                     
-        #maxIndex = e.index(max(e))
+        maxIndex = e.index(max(e))
         
+        print "\n"
         print "Get"
         print c[maxIndex]
         print "at a price of"
@@ -57,19 +119,10 @@ class BeerRunSpider(Spider):
         print "for"
         print e[maxIndex]
         print "ml alcohol/$"
+        print "\n"
         
-        client = MongoClient()
-        db = client.mydb
-        collection = client.beer
-        
-        beer = { "list": c }
-        
-        db.collection.insert(beer)
-        
-        print beer
         
         for item in c:
-            
             print item
         print e
             
