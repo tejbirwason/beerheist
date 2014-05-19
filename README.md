dd4d0c6b050893af86099ef6b2fc3832b5be3d87
tejbirwason <tejbirwason@gmail.com>
2014-05-18 20:38:41 -0400

    all entries update

---
+++ README.md
@@ -0,0 +1,3 @@
+beerheist
+=========
+Scraping thebeer.ca to find the cheapest drinking options (by alcohol per unit dollar)
--- beerrun/spiders/__init__.py
+++ beerrun/spiders/__init__.py
@@ -16,13 +16,12 @@ class BeerRunSpider(Spider):
     allowed_domains = ["thebeerstore.ca"]
     start_urls = [
         "http://www.thebeerstore.ca/stores"
+        #"http://www.thebeerstore.ca/beers/inventory/574/4004"
     ]
 
 
     def parse(self, response):
         soup = BeautifulSoup(response.body)  
-        store_name=[]
-        store_id=[]
 
         ## mongo stuff
         client = MongoClient()
@@ -30,102 +29,46 @@ class BeerRunSpider(Spider):
         beerrun = client.beerrun
 
 
-        for link in soup.find_all("a","store-link"):
-            extractStoreName = link.get_text() 
-            store_name.append(extractStoreName)
-            extractID = re.findall(r'\d+',str(re.findall(r'# \d+',str(link.parent()))))[0]
-            store_id.append(extractID)
-            
-            #print beer
-            showcaseURL = "http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id="+extractID
-            soup = BeautifulSoup((requests.get(showcaseURL)).text)
-            count = 0
-            for link in soup.find_all("a","brand-link teaser"):
-                beerinventoryURL = "http://www.thebeerstore.ca"+link.get('href')
-                imageURL = (link.find_all("img","image-style-none")[0]).get('src')
-                #print(link.get('href'))
-                beer = { "store_name": extractStoreName,
-                    "store_id": extractID,
-                    "showcase_url":showcaseURL,
-                    "beer_invetory_url": beerinventoryURL
-                    "brand_image_url": imageURL}
-                #db.beerrun.insert(beer)
-            #print re.findall(r'\d+',str(re.findall(r'# \d+',str(link.parent()))))[0]
+        for storelink in soup.find_all("a","store-link"):
+            extractStoreName = storelink.get_text() 
+            extractID = re.findall(r'\d+',str(re.findall(r'# \d+',str(storelink.parent()))))[0]
+            if (extractID=="4616"): #will be removed later
+                showcaseURL = "http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id="+extractID
+                soup = BeautifulSoup((requests.get(showcaseURL)).text)
+                for link in soup.find_all("a","brand-link teaser"):
+                    beerinventoryURL = "http://www.thebeerstore.ca"+link.get('href')
+                    imageURL = (link.find_all("img","image-style-none")[0]).get('src')
+                    beername = (link.find("span","brand-name")).get_text()
+                    print beername
+                    soup2 = BeautifulSoup((requests.get(beerinventoryURL)).text)  
+                    for row in soup2.find_all("tr",["odd","even"]):
+                        size = (row.find("td","size")).get_text()
+                        price = (row.find("td","price")).get_text()
+                        inventory = (row.find("td","inventory")).get_text()
+                        temp = re.findall(r'\d+',size)
+                        quantity = int(temp[0])*int(temp[1])
+                        temp2 = price.strip('$')
+                        if temp2.find("sale")==-1:
+                            strippedPrice = (float(temp2))
+                            onSale = 0
+                        else:
+                            strippedPrice = (float(temp2.split('$')[1]))
+                            onSale = 1
+                        if inventory == "0":
+                            apd = 0
+                        else:
+                            apd = quantity/strippedPrice
 
-        #print db.collection.find()
-        #break
-        #print store_name
-        #print store_id
-
-        #for storeIdAppendLink in store_id:
-            #beerlist = []
-           # returned = requests.get("http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id="+str(storeIdAppendLink))
-          #  soup = BeautifulSoup(returned.text)
-            #for link in soup.find_all("a","brand-link teaser"):
-                #print(link.get('href'))
-
-
-
-    def here(self, response):
-        print "here"
-
-        #r = requests.get("http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id=4607")
-        #print(r.text)
-        #print(r.status_code)
-        #soup.find_all('a',)
-
-        #tryin = Request(url="http://www.thebeerstore.ca/sites/all/modules/custom/tbs_api/modules/tbs_api_stores/json.php?store_id=4607",
-                              # cookies={'currency': 'USD', 'country': 'UY'})
-        #print(soup.prettify())
-
-    def parse2(self, response):
-        soup = BeautifulSoup(response.body)        
-        a=[]
-        b=[]
-        c=[]
-        d=[]
-        for size in soup.find_all("td","size"):
-            text = size.get_text()
-            c.append(text)
-            m = re.findall(r'\d+',text)
-            a.append(int(m[0])*int(m[1]))
-        for price in soup.find_all("td","price"):
-            price = price.get_text().strip('$')
-            if price.find("sale")==-1:
-                b.append(float(price))
-            else:
-                b.append(float(price.split('$')[1]))
-        for inventory in soup.find_all("td","inventory"):
-            d.append(int(inventory.get_text()))
-        
-        e=[]
-        
-        assert len(a) == len(b) == len(c)
-        
-
-        for i in range(len(a)):
-                if d[i]==0:
-                    e.append(0)
-                else:
-                    e.append(a[i]/b[i])
-                    
-        maxIndex = e.index(max(e))
-        
-        print "\n"
-        print "Get"
-        print c[maxIndex]
-        print "at a price of"
-        print b[maxIndex]
-        print "for"
-        print e[maxIndex]
-        print "ml alcohol/$"
-        print "\n"
-        
-        
-        for item in c:
-            print item
-        print e
-            
-            
-            
-            
+                        beer = { "store_name": extractStoreName,
+                            "store_id": extractID,
+                            "showcase_url":showcaseURL,
+                            "beer_invetory_url": beerinventoryURL,
+                            "brand_image_url": imageURL,
+                            "beer_name": beername,
+                            "size": size,
+                            "volume": quantity,
+                            "price": strippedPrice,
+                            "inventory":inventory,
+                            "onSale":onSale,
+                            "apd":apd}
+                        db.beerrun.insert(beer)
